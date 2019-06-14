412acb4721443fdf5592861873a1ad5f66c58648
Ezequiel Lopez <lopezezequiel9611@gmail.com>
2019-06-13 21:36:36 -0300

    Agrege archivos y otros

--- Obligatorio2P2/src/Interfaz/MenuPrincipal.java
+++ Obligatorio2P2/src/Interfaz/MenuPrincipal.java
@@ -13,10 +13,12 @@ import obligatorio2p2.*;
 public class MenuPrincipal extends javax.swing.JFrame {
     public Aves a;
     public String hola;
+    boolean onTop;
     public MenuPrincipal() {
         initComponents();
         this.setTitle("Aves v2.0 Beta");
         a = new Aves();
+        onTop=false;
         this.setResizable(false);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
@@ -42,6 +44,9 @@ public class MenuPrincipal extends javax.swing.JFrame {
         config = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         texto = new javax.swing.JLabel();
+        diferentes = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        salir = new javax.swing.JButton();
         fondoImg = new javax.swing.JLabel();
 
         jButton4.setText("Configuracion");
@@ -65,7 +70,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jugar);
-        jugar.setBounds(134, 243, 267, 29);
+        jugar.setBounds(140, 130, 267, 29);
 
         registro.setText("¡Registrate!");
         registro.addActionListener(new java.awt.event.ActionListener() {
@@ -74,7 +79,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
             }
         });
         getContentPane().add(registro);
-        registro.setBounds(134, 318, 267, 29);
+        registro.setBounds(140, 210, 267, 29);
 
         config.setText("Configuración");
         config.addActionListener(new java.awt.event.ActionListener() {
@@ -83,7 +88,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
             }
         });
         getContentPane().add(config);
-        config.setBounds(134, 390, 267, 29);
+        config.setBounds(140, 280, 267, 29);
         getContentPane().add(jLabel2);
         jLabel2.setBounds(0, 0, 0, 0);
 
@@ -93,6 +98,24 @@ public class MenuPrincipal extends javax.swing.JFrame {
         texto.setText("AVES 2.0");
         getContentPane().add(texto);
         texto.setBounds(130, 10, 290, 130);
+
+        diferentes.setText("Diferencia de nombres");
+        diferentes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                diferentesActionPerformed(evt);
+            }
+        });
+        getContentPane().add(diferentes);
+        diferentes.setBounds(290, 330, 170, 100);
+
+        jButton2.setText("Puntuaciones");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(80, 330, 170, 100);
+
+        salir.setBackground(new java.awt.Color(255, 51, 51));
+        salir.setText("Salir");
+        getContentPane().add(salir);
+        salir.setBounds(210, 470, 120, 29);
 
         fondoImg.setIcon(new javax.swing.ImageIcon("/Users/ezequiellopez/Desktop/Dibujo20140712-upside-down-bird-on-cable-national-geographic-hideta-nagai-1024x768.jpg")); // NOI18N
         getContentPane().add(fondoImg);
@@ -107,10 +130,6 @@ public class MenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jugarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jugarActionPerformed
-
-        
-           System.out.println("Flag config jug "+ a.getConfiguracion()[0]);
-           System.out.println("Flag cant jug "+a.getJugadores().size());
         if(a.getConfiguracion()[0]<=a.getJugadores().size()){
             SeleccionarJugadores panel=new SeleccionarJugadores(a);
         panel.setVisible(true); 
@@ -129,6 +148,17 @@ public class MenuPrincipal extends javax.swing.JFrame {
         Configuracion config=new Configuracion(a);
         config.setVisible(true);
     }//GEN-LAST:event_configActionPerformed
+
+    private void diferentesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_diferentesActionPerformed
+        if(!onTop){
+        SelectArch dif=new SelectArch();
+        dif.setVisible(true);
+        onTop=true;
+        }else{
+            
+        }
+        
+    }//GEN-LAST:event_diferentesActionPerformed
 
     /**
      * @param args the command line arguments
@@ -167,13 +197,16 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton config;
+    private javax.swing.JButton diferentes;
     private javax.swing.JLabel fondoImg;
+    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JButton jugar;
     private javax.swing.JButton registro;
+    private javax.swing.JButton salir;
     private javax.swing.JLabel texto;
     // End of variables declaration//GEN-END:variables
 }
