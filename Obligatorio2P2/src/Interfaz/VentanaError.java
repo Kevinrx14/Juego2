cfe554bb9923270a0a3a670834a1da3c4cd5ea92
Kevinrx <kevinrx14@gmail.com>
2019-06-21 00:53:11 -0300

    Filtros agregados en registro de jugadores

--- Obligatorio2P2/src/Interfaz/VentanaError.java
+++ Obligatorio2P2/src/Interfaz/VentanaError.java
@@ -77,7 +77,7 @@ public class VentanaError extends javax.swing.JDialog {
         texto.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         texto.setText("ERROR");
         getContentPane().add(texto);
-        texto.setBounds(10, 60, 394, 26);
+        texto.setBounds(10, 16, 360, 70);
 
         jButton1.setText("Aceptar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -86,7 +86,7 @@ public class VentanaError extends javax.swing.JDialog {
             }
         });
         getContentPane().add(jButton1);
-        jButton1.setBounds(158, 100, 92, 29);
+        jButton1.setBounds(125, 90, 130, 32);
         getContentPane().add(jLabel1);
         jLabel1.setBounds(70, 60, 0, 0);
         getContentPane().add(icono);
